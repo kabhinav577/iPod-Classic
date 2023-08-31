d861c228f42c6ed6eddde661439bdc8b9c544454
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-08-31 09:33:10 +0530

    Creating function and state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import "../css/App.css";
 
 // Importing Components
 import KnowMore from "./KnowMore";
+import Case from "./Case";
 
 // Importing all Songs
 import song1 from "../static/songs/ram-siya-ram.mp3";
@@ -85,7 +86,7 @@ class App extends React.Component {
           this.state.audio.play();
         }
       );
-    } else if (e.detail.interval > 250 && e.detail.interval < 1000) {
+    } else if (e.detail.interval > 250 && e.detail.interval < 10000) {
       const interval = e.detail.interval / 100;
       this.setState((prevState) => {
         prevState.audio.currentTime += interval;
@@ -94,10 +95,292 @@ class App extends React.Component {
     }
   };
 
+  // FUNCTION FOR :: ON LONG PRESS ON BACKWARD BTN TRACKS ARE SEEKED BACKWARD
+  seekSongReverse = (e) => {
+    if (this.state.currentMenu === -2) return;
+
+    if (this.state.playing === false) return;
+
+    if (e.detail.interval < 250) {
+      this.state.audio.pause();
+      let songIndex = this.state.songIndex;
+      if (songIndex === this.state.songItemsUrl.length - 1) {
+        songIndex = this.state.songItemsUrl.length - 1;
+      } else {
+        songIndex--;
+      }
+
+      const songUrl = this.state.songItemsUrl[songIndex];
+      const songImgUrl = this.state.songImgItemsUrl[songIndex];
+      this.setState(
+        {
+          songIndex: songIndex,
+          songUrl: songUrl,
+          songImgUrl: songImgUrl,
+          audio: new Audio(songUrl),
+        },
+        () => {
+          this.state.audio.play();
+        }
+      );
+    } else if (e.detail.interval > 250 && e.detail.interval < 10000) {
+      const interval = e.detail.interval / 100;
+      this.setState((prevState) => {
+        prevState.audio.currentTime -= interval;
+        return prevState;
+      });
+    }
+  };
+
+  // TOGGLE BUTTON FOR PLAY OR PAUSE ->
+  togglePlayPause = () => {
+    if (this.state.currentMenu === -2) return;
+
+    if (this.state.playing === true) {
+      this.setState({ playing: false });
+      this.state.audio.pause();
+    } else {
+      this.setState({ playing: true });
+      this.state.audio.play();
+    }
+  };
+
+  // FUNCTION FOR :: UPDATE ACTIVE MENU WHILE ROTATING ON THE TRACK-WHEEL
+  updateActiveMenu = (direction, menu) => {
+    if (
+      menu !== -1 &&
+      menu !== 1 &&
+      menu !== 4 &&
+      menu !== 8 &&
+      menu !== 3 &&
+      menu !== 9 &&
+      menu !== 10
+    )
+      return;
+
+    let min = 0,
+      max = 0;
+
+    max = this.state.lengthMenuKey[menu];
+
+    if (direction === 1) {
+      if (this.state.active >= max) {
+        this.setState({ active: min });
+      } else {
+        this.setState({ active: this.state.active + 1 });
+      }
+    } else {
+      if (this.state.active <= min) {
+        this.setState({ active: max });
+      } else {
+        this.setState({ active: this.state.active - 1 });
+      }
+    }
+  };
+
+  // FUNCTION FOR :: CHANGE THE TEHEME OF iPod BODY
+  setTheme = (id) => {
+    let theme = "";
+    if (id === 0) theme = "#FDDCD7";
+    else if (id === 1) theme = "rgb(210, 210, 210)";
+    else if (id === 2) theme = "#F5DDC5";
+    else if (id === 3) theme = "#D1CDDA";
+    else if (id === 4) theme = "black";
+
+    this.setState({ theme: theme, noty: true, notifyText: "Theme Changed" });
+    return;
+  };
+
+  // FUNCTION FOR :: WHEEL COLOR CHANGED
+  setWheelColor = (id) => {
+    let wheelColor = "";
+    if (id === 0) wheelColor = "#212121";
+    else if (id === 1) wheelColor = "white";
+    else if (id === 2) wheelColor = "#3E2723";
+    else if (id === 3) wheelColor = "#3D5AFE";
+    this.setState({
+      wheelColor: wheelColor,
+      noty: true,
+      notifyText: "Wheel Color Changed",
+    });
+    return;
+  };
+
+  // FUNCTION FOR :: SET WALLPAPER OF IPOD SCREEN
+  setWallpaper = (id) => {
+    this.setState({
+      wallpaper: id,
+      noty: true,
+      notifyText: "Wallpaper Changed!",
+    });
+    return;
+  };
+
+  // FUNCTION FOR :: CHANGE PLAYING MUSIC FORM MENU
+  changePlayingSongFromMenu = (id, navigationStack) => {
+    const songUrl = this.state.songItemsUrl[id];
+    const songImgUrl = this.state.songImgItemsUrl[id];
+    this.state.audio.pause();
+    this.setState(
+      {
+        currentMenu: 7,
+        songUrl: songUrl,
+        navigationStack: navigationStack,
+        active: 0,
+        playing: true,
+        songIndex: id,
+        audio: new Audio(songUrl),
+        songImgUrl: songImgUrl,
+      },
+      () => {
+        this.state.audio.play();
+      }
+    );
+
+    return;
+  };
+
+  // FUNCTION FOR :: CHANGE MENU FORWARD on PRESS OF CENTER BTN
+  changeMenuForward = (id, fromMenu) => {
+    const navigationStack = this.state.navigationStack.slice();
+
+    if (
+      fromMenu !== -2 &&
+      fromMenu !== -1 &&
+      fromMenu !== 1 &&
+      fromMenu !== 4 &&
+      fromMenu !== 3 &&
+      fromMenu !== 8 &&
+      fromMenu !== 9 &&
+      fromMenu !== 0 &&
+      fromMenu !== 7 &&
+      fromMenu !== 10
+    )
+      return;
+
+    if (fromMenu === -2) {
+      navigationStack.push(this.state.currentMenu);
+      this.setState({
+        currentMenu: -1,
+        navigationStack: navigationStack,
+        active: 0,
+      });
+      return;
+    }
+
+    if (fromMenu === -1) {
+      navigationStack.push(this.state.currentMenu);
+      this.setState({
+        currentMenu: id,
+        navigationStack: navigationStack,
+        active: 0,
+      });
+    }
+
+    if (fromMenu === 7 || fromMenu === 0) {
+      this.togglePlayPause();
+      return;
+    }
+
+    if (fromMenu === 8) {
+      this.setTheme(id);
+      return;
+    }
+
+    if (fromMenu === 9) {
+      this.setWheelColor(id);
+      return;
+    }
+
+    if (fromMenu === 10) {
+      this.setWallpaper(id);
+      return;
+    }
+
+    navigationStack.push(this.state.currentMenu);
+
+    if (fromMenu === 4) {
+      this.changePlayingSongFromMenu(id, navigationStack, fromMenu);
+      return;
+    }
+
+    const currentMenuID = this.state.menuMapping[fromMenu][id];
+    this.setState({
+      currentMenu: currentMenuID,
+      navigationStack: navigationStack,
+      active: 0,
+    });
+    return;
+  };
+
+  // FUNCTION FOR ::CHANGE MENU BACKWARDS on PRESS CENTER OF BTN
+  changeMenuBackward = () => {
+    const navigationStack = this.state.navigationStack.slice();
+
+    if (this.state.currentMenu === -2) return;
+    else {
+      const prevID = navigationStack.pop();
+      this.setState({
+        currentMenu: prevID,
+        navigationStack: navigationStack,
+        active: 0,
+      });
+      return;
+    }
+  };
+
+  // FUNCTION FOR :: SET NOTIFICATION WHEN SENDING NOTIFICATION
+  setNoty = () => {
+    this.setState({ noty: false });
+    return;
+  };
+
   render() {
+    const {
+      audio,
+      active,
+      currentMenu,
+      menuItems,
+      musicItems,
+      songItems,
+      playing,
+      songIndex,
+      theme,
+      songUrl,
+      songImgUrl,
+      wheelColor,
+      wallpaper,
+      wallpaperItems,
+      noty,
+      notifyText,
+    } = this.state;
     return (
       <div>
         <KnowMore />
+        <Case
+          songIndex={songIndex}
+          active={active}
+          menuItems={menuItems}
+          currentMenu={currentMenu}
+          audio={audio}
+          musicItems={musicItems}
+          songItems={songItems}
+          playing={playing}
+          theme={theme}
+          songUrl={songUrl}
+          songImgUrl={songImgUrl}
+          wheelColor={wheelColor}
+          wallpaper={wallpaper}
+          wallpaperItems={wallpaperItems}
+          noty={noty}
+          notifyText={notifyText}
+          changeMenuForward={this.changeMenuForward}
+          changeMenuBackward={this.changeMenuBackward}
+          updateActiveMenu={this.updateActiveMenu}
+          togglePlayPause={this.togglePlayPause}
+          seekSongForward={this.seekSongForward}
+          seekSongReverse={this.seekSongReverse}
+        />
       </div>
     );
   }
---
+++ src/components/Case.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Case extends React.Component {
+  render() {
+    return <h1>Case Compo</h1>;
+  }
+}
+
+export default Case;
